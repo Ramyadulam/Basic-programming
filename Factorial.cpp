44c1b047101bf8c84a42d37ea6be69d9351b3b8e
Ramya Dulam <155840420+Ramyadulam@users.noreply.github.com>
2024-01-31 22:32:33 +0530

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -16,3 +16,18 @@ int main(){
         cout<<"factorial of"<<n<<" "<<factorial;
     }
 }
+//using while loop:
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    long factorial=1;
+    cout<<"Enter the positive integer: ";
+    int i=1;
+    while(i<=n){
+        factorial *= i;
+        i++;
+    }
+    cout<<"factorial"<<n<<" "<<factorial;
+}
