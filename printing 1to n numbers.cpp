3a3bc44bde48c0d5693cc06755a91c4511399fa9
Ramya Dulam <155840420+Ramyadulam@users.noreply.github.com>
2024-01-31 21:16:49 +0530

    Create printing 1to n numbers.cpp

---
+++ printing 1to n numbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        cout<<i<<" ";
+    }
+}
+//reverse of 1ton numbers:
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+for(int i=0;i<=n;i--){
+  cout<<i<<" "<<endl;
+}
