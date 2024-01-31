8cce891d713fd175c245ce9855510c57fd4b0bab
Ramya Dulam <155840420+Ramyadulam@users.noreply.github.com>
2024-01-31 21:24:45 +0530

    Update printing 1to n numbers.cpp

--- printing 1to n numbers.cpp
+++ printing 1to n numbers.cpp
@@ -17,3 +17,17 @@ cin>>n;
 for(int i=0;i<=n;i--){
   cout<<i<<" "<<endl;
 }
+//using while loop:
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the numbers: ";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        cout<<i<<" "<<endl;
+        i++;
+    }
+}
