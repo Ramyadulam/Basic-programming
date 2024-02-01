420deb8f61d2ebd41c8b650a3d8c7644dd298d15
Ramya Dulam <155840420+Ramyadulam@users.noreply.github.com>
2024-02-01 21:20:53 +0530

    Update Is_prime.cpp

--- Is_prime.cpp
+++ Is_prime.cpp
@@ -21,4 +21,24 @@ int main()
     else{
         cout<<n<<"not a prime";
     }
+
+    //by using functions
+    #include<bits/stdc++.h>
+using namespace std;
+int prime(int n)
+{
+    for(int i=2;i<=sqrt(n);i++)
+    {
+        if(n%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<prime(n);
+}
 }
